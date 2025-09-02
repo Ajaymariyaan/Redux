d2eac1b593952be7ed03bee749da107c13d2de6c
Ajay <ajayantony9296@gmail.com>
2025-09-02 18:39:58 +0530

    Other Files and Packages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import "@/styles/globals.css";
+import { Provider } from "react-redux";
+import store from "@/store/Store";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
